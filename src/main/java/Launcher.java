909bc355e3eb91c9224179f7cf2a8f4a682d2ebf
Alexey <Switch_h13@mail.ru>
2018-11-06 15:00:42 +0300

    Binding loger error fixed

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,16 +1,15 @@
 package main.java;
 
 import main.java.model.beans.SeekerDescription;
-
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 import main.java.presenter.JavaTestPresenter;
 import main.java.view.HTMLCreater;
 import main.java.view.HTMLCreaterImpl;
-import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.log4j.PropertyConfigurator;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by PalibinFamily on 11.06.2018.
@@ -19,8 +18,10 @@ import java.io.FileNotFoundException;
 public class Launcher {
     public static void main(String[] args) {
         try {
-            PropertyConfigurator.configure("log4j.properties");
-        } catch (NullPointerException e) {
+            String filepath= "log4j.properties";
+            FileInputStream testStream = new FileInputStream(filepath);
+            PropertyConfigurator.configure(filepath);
+        } catch (FileNotFoundException e) {
             System.out.println("there is no external config for log4j. Binding default internal config.");
             System.out.println(System.getProperty("java.class.path"));
             PropertyConfigurator.configure(Launcher.class.getResourceAsStream("/resources/log4j.properties"));
